635e373dfe37d3a30aa85e17903ce5dc0f1485e8
james-cosgrove <cosgrove.jamesc@gmail.com>
2017-01-27 15:06:41 +1100

    initialize gulpfile to implement browsersync and automate compiling of scss into css

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
+
+const sassPath = './app/scss/**/*.scss';
+const scriptPath = './app/javascript/app.js';
+const htmlPath = './app/index.html';
+
+
+// compile scss into css
+gulp.task('sass-to-css', function() {
+  return gulp.src(sassPath)
+    .pipe(sass())
+    .pipe(gulp.dest('./app/css'))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('reload', function() {
+  browserSync.reload();
+});
+
+// watches for any saved changes on js, html and scss files
+gulp.task('watch-for-changes', ['sass-to-css'], function() {
+  gulp.watch(sassPath, ['sass-to-css']);
+  gulp.watch(scriptPath, ['reload']);
+  gulp.watch(htmlPath, ['reload']);
+});
