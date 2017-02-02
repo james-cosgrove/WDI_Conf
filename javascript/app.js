8efc9b7362d90b1beab6faa96cf12db25396888b
chroc <chroc7@gmail.com>
2017-02-02 13:52:39 +1100

    chat window working with the bot

--- javascript/app.js
+++ javascript/app.js
@@ -26,6 +26,46 @@ $(document).ready(function(){
     });
   });
 
+  var socket = io('https://shrouded-everglades-13835.herokuapp.com/');
+
+  var sendMessage = function(){
+    var message = $("#btn-input").val();
+    console.log("message: " + message);
+    socket.emit('chat message', message);
+    $("#chatbox").append(
+      '<li class="right clearfix"><span class="chat-img pull-right">' +
+      '<img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="User Avatar" class="img-circle" /></span>' +
+      '<div class="chat-body clearfix">' +
+      '<div class="header">' +
+      '<small class=" text-muted"><span class="glyphicon glyphicon-time"></span></small>' +
+      '<strong class="pull-right primary-font">User</strong></div>' +
+      '<p>' + message + '</p></div></li>'
+    );
+    $("#btn-input").val("");
+    return false;
+  }
+
+  $("#btn-chat").bind("click", sendMessage);
+
+  $("#send-message").submit(sendMessage);
+
+  socket.on('chat response', function(msg) {
+    console.log("message back: " + msg);
+    // $("#chatbox").append("<p>" + msg + "</p>");
+    $("#chatbox").append(
+      '<li class="left clearfix"><span class="chat-img pull-left">' +
+          '<img src="http://placehold.it/50/55C1E7/fff&text=U" alt="User Avatar" class="img-circle" />' +
+      '</span>' +
+          '<div class="chat-body clearfix">' +
+              '<div class="header">' +
+                  '<strong class="primary-font">Bot</strong> <small class="pull-right text-muted">' +
+                      '<span class="glyphicon glyphicon-time"></span></small>' +
+              '</div>' +
+              '<p>' + msg + '</p></div></li>'
+    );
+    $("#btn-input").val("");
+  });
+
   function init() {
       window.addEventListener('scroll', function(e){
           var distanceY = window.pageYOffset || document.documentElement.scrollTop;
@@ -41,7 +81,5 @@ $(document).ready(function(){
       });
   }
   window.onload = init();
-
-
 
 })
